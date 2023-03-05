8fe67850baaa8bef4b2325d4af3c024535ae0f30
olensi42 <oleczyk@protonmail.com>
2023-03-05 15:26:24 +0100

    adjusted for new parser

--- src/main.cpp
+++ src/main.cpp
@@ -5,15 +5,12 @@
 int main(int argc, char const *argv[])
 {
     // ldraw library path
-    LDrawConverter converter("C:/Users/Ole/Documents/Assets/Lego/ldraw/");
+    LDrawConverter converter("LIBRARY_PATH/");
     
-    LDrawFile* file = converter.ParseFile("C:/Users/Ole/Desktop/L/X-Wing.ldr");
-    // LDrawFile* file = converter.ParseFile("C:/Users/Ole/Desktop/L/2x2.ldr");
+    LDrawFile* file = converter.ParseFile("IMPORT_PATH/FILE.ldr");
     std::cout << "MeshCount: " << converter.meshCount << std::endl;
 
     LDrawExporter exporter(&converter);
-    //exporter.LoadParts();
-
     exporter.Export(file, "C:/Users/Ole/Desktop/a/newXwing.fbx");
 
     delete file;
