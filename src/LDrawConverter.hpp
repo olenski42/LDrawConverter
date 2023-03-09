10d91deee15f95caa39651673a2317bab87fd000
olensi42 <oleczyk@protonmail.com>
2023-03-09 18:10:02 +0100

    formating

--- src/LDrawConverter.hpp
+++ src/LDrawConverter.hpp
@@ -7,27 +7,24 @@
 #include "Log.hpp"
 #include "LDrawFile.hpp"
 
-
 inline unsigned int convSizetUint(size_t size);
 inline int convSizetInt(size_t size);
-
 
 class LDrawConverter
 {
 public:
-    LDrawConverter(const char* libraryPath);
+    LDrawConverter(const char *libraryPath);
 
-    std::map<std::string, LDrawFile*> nameResolver;
+    std::map<std::string, LDrawFile *> nameResolver;
     std::vector<UnresolvedFile> unresolvedFiles;
 
-
     /* @param file the file it should fill with data
-    *  @param filePath the absolute filepath
-    *  @return whether the file could be parsed. Only use the file if this returns true!
-    */
-    LDrawFile* ParseFile(std::string filePath);
-    std::ifstream FindFile(UnresolvedFile* file);
-    LDrawFile* GetFile(std::string fileName, FileType fileType);
+     *  @param filePath the absolute filepath
+     *  @return whether the file could be parsed. Only use the file if this returns true!
+     */
+    LDrawFile *ParseFile(std::string filePath);
+    std::ifstream FindFile(UnresolvedFile *file);
+    LDrawFile *GetFile(std::string fileName, FileType fileType);
 
     void LoadColorFile();
     std::map<ColorID, LDrawColor> colorMap;
@@ -36,7 +33,7 @@ public:
 
 private:
     void ResolveAll();
-    bool ParseFile(LDrawFile* file, std::ifstream& fileStream, FileType parentType = FILETYPE_MULTIPART);
-    
+    bool ParseFile(LDrawFile *file, std::ifstream &fileStream, FileType parentType = FILETYPE_MULTIPART);
+
     std::string libPath;
 };
