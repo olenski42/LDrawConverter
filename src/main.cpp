b0a6eec543954c37de4fffae6b0a41283d5fa0b6
olensi42 <oleczyk@protonmail.com>
2023-03-13 04:33:29 +0100

    added file dialog

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,29 @@
 #include "LDrawConverter.hpp"
 #include "LDrawExporter.hpp"
 #include "MeshData.hpp"
+#include "portable-file-dialogs.h"
+#include <string>
 
 int main(int argc, char const *argv[])
 {
     // ldraw library path
-    LDrawConverter converter("LIBRARY_PATH/");
-    
-    LDrawFile* file = converter.ParseFile("IMPORT_PATH/FILE.ldr");
+    LDrawConverter converter("C:/Program Files/Studio 2.0/ldraw/");
+
+    pfd::open_file fileDialog("Select LDraw file", ".", {"LDraw files (.ldr .mpd)", "*.ldr *.mpd", "All files", "*"});
+    if (fileDialog.result().size() == 0)
+        return 0;
+
+    std::string filePath = fileDialog.result()[0];
+    LDrawFile *file = converter.ParseFile(filePath);
+
     std::cout << "MeshCount: " << converter.meshCount << std::endl;
 
     LDrawExporter exporter(&converter);
-    exporter.Export(file, "C:/Users/Ole/Desktop/a/newXwing.fbx");
+
+    
+    std::string outPath = pfd::save_file("Save FBX", ".", {"FBX files", "*.fbx", "All files", "*"}).result();
+    exporter.Export(file, outPath);
+    
 
     delete file;
     return 0;
